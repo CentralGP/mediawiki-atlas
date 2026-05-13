49fcd3eea060a52ec62e49eb17daadca09df6c7e
Sean Turecek <sturecek@central.com>
2026-05-13 09:04:40 -0700

    feat: outline folder for checkmk monitoring page

---
+++ checkmk-status/index.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Checkmk Status Test</title>
+</head>
+<body>
+  <h1>Checkmk status page is reachable</h1>
+  <p>If you can see this, the PHP iframe target path works.</p>
+</body>
+</html>
